0771ff7733619b3dc00f58ae90c956285b973fe8
Justen Walker <justen.walker+github@gmail.com>
2021-10-03 10:54:45 -0400

    add support for hstr

--- .zsh/zshrc.d/10-history.zsh
+++ .zsh/zshrc.d/10-history.zsh
@@ -1,7 +1,13 @@
 # History
-setopt histignorealldups sharehistory
+setopt histignorealldups sharehistory histignorespace
 
 # Keep 1000 lines of history within the shell and save it to ~/.zsh_history:
 HISTSIZE=1000
 SAVEHIST=1000
 HISTFILE=~/.zsh_history
+
+if command -v hstr > /dev/null; then
+  alias hh=hstr
+  export HSTR_CONFIG=hicolor
+  bindkey -s "\C-r" "\C-a hstr -- \C-j"
+fi
